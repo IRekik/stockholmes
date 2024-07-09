09406bd297bebe460718289469dc35b980cc9351
AdamGhieh <ghiehadam123@gmail.com>
2024-07-08 23:31:55 -0400

    Changed header color to black

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="text-center">
       <div className="m-10">
-        <h1 className="text-3xl">Welcome to StockHolmes</h1>
+        <h1 className="text-3xl text-black">Welcome to StockHolmes</h1>
       </div>
       <div className="text-red-400 p-5">
         <p>A game website where you can challenge yourself in a virtual stock market</p>
