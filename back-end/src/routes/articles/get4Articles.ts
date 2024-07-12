c9c2aade6e0ca646f262f43328d65867e1249130
abdellah240 <155846993+abdellah240@users.noreply.github.com>
2024-07-12 17:15:06 -0400

    Create get4Articles.ts

---
+++ back-end/src/routes/articles/get4Articles.ts
@@ -0,0 +1,26 @@
+import  express  from "express";
+import { Article } from "../../types/db/types";
+import knexInstance from "../../utils/db";
+
+const router = express.Router();
+router.get("/fourArticles", async (req, res) => {
+    try{ 
+        // Query the database using Knex
+        const result: Article[] = await knexInstance("articles") // array defined as result awaits "articles" table
+        .select('*') // Selects all columns ("id","title", etc.)
+        .orderBy("creation_date", "desc")
+        .limit(4) ; // ; here??
+        
+        if(result.length==0){
+        throw new Error ("No data found")
+        }
+      
+        console.log("4 articles retrieved successfully from the database", result)
+        res.json(result) // Sends response to client
+
+    }catch(error){
+        console.log("Error retrieving data from the database:", error)
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+export default router;
